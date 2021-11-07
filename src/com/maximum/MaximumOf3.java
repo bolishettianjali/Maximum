01ad4719ea75ac03a5a6146b1fe0d6e67657d888
bolishettianjali <bolishettianjali@gamil.com>
2021-11-07 12:36:40 +0530

    Finding Max of String

--- src/com/maximum/MaximumOf3.java
+++ src/com/maximum/MaximumOf3.java
@@ -3,9 +3,10 @@ package com.maximum;
 public class MaximumOf3 {
     public static void main(String[] args){
         System.out.println("Welcome to Program of finding maximum ");
-        Float s1=10.0f;
-        Float s2=20.0f;
-        Float s3=30.0f;
+        String s1="apple";
+        String s2="banana";
+        String s3="peach";
+
         System.out.println(getMax(s1,s2,s3));
         }
         private static <T extends Comparable> T getMax(T a,T b,T c){
