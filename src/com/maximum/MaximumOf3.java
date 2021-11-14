4928181d657df57af48ea11dcbd4c2ec83345681
bolishettianjali <bolishettianjali@gamil.com>
2021-11-14 16:59:14 +0530

    Added: Generic Method to print Maximum values

--- src/com/maximum/MaximumOf3.java
+++ src/com/maximum/MaximumOf3.java
@@ -1,20 +1,47 @@
 package com.maximum;
 
-public class MaximumOf3 {
-    public static void main(String[] args){
-        System.out.println("Welcome to Program of finding maximum ");
-        String s1="apple";
-        String s2="banana";
-        String s3="peach";
-
-        System.out.println(getMax(s1,s2,s3));
+public class MaximumOf3<T extends Comparable<T>> {
+    T x,y,z;
+    public MaximumOf3(T x,T y,T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public T maximum() {
+        return MaximumOf3.maximum(x,y,z);
+    }
+    public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
+        T max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
         }
-        private static <T extends Comparable> T getMax(T a,T b,T c){
-        if(a.compareTo(b)>0 && a.compareTo(c)>0)
-            return a;
-          else  if(a.compareTo(b)>0 && a.compareTo(c)>0)
-                return b;
-          else
-              return c;
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        printMax(x, y, z, max);
+        return max;
+    }
+    public static String testMaximum(String x,String y,String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        printMax(x, y, z, max);
+        return max;
+    }
+      public static <T> void printMax(T x,T y,T z,T max){
+        System.out.printf("Max of %s,%s and %s is %s\n",x,y,z,max);
+    }
+    public static void main(String[] args){
+        Integer xInt=3,yInt=4,zInt=5;
+        Float xF1=6.6f,yF1=8.8f,zF1=7.7f;
+        String xStr="pear",yStr="apple",zStr="orange";
+        MaximumOf3.testMaximum(xStr,yStr,zStr);
+        new MaximumOf3<>(xInt,yInt,zInt).maximum();
+        new MaximumOf3<>(xF1,yF1,zF1).maximum();
+        new MaximumOf3<>(xStr,yStr,zStr).maximum();
     }
 }
